4f5cbc07c57fa5572236c01f599a57ea852e814c
Matt Hayes <matt@mysterycommand.com>
2020-03-08 06:26:18 -0400

    feat: roll a die

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,31 +1,35 @@
-import React, { FC } from 'react';
+import React, { FC, useReducer } from 'react';
 
 import './App.css';
 
-export const App: FC = () => (
-  <div className="App">
-    <div className="App-controls">
-      <ul className="App-dice">
-        <li>
-          <button>d20</button>
-        </li>
-        <li>
-          <button>d12</button>
-        </li>
-        <li>
-          <button>d10</button>
-        </li>
-        <li>
-          <button>d8</button>
-        </li>
-        <li>
-          <button>d6</button>
-        </li>
-        <li>
-          <button>d4</button>
-        </li>
-      </ul>
-      <button>clear</button>
+const { floor, random } = Math;
+const roll = (sides: number) => floor(random() * sides) + 1;
+const actions = {
+  roll,
+};
+
+const initialState = 0;
+const reducer = (state: {}, { type, sides }: { type: 'roll'; sides: number }) =>
+  actions[type] ? actions[type](sides) : state;
+
+export const App: FC = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className="App">
+      <div className="App-value">{state}</div>
+      <div className="App-controls">
+        <ul className="App-dice">
+          {[20, 12, 10, 8, 6, 4].map(sides => (
+            <li>
+              <button onClick={() => dispatch({ type: 'roll', sides })}>
+                d{sides}
+              </button>
+            </li>
+          ))}
+        </ul>
+        <button>clear</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
