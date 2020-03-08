752c834f50b19b64618fe1f2745d79a395d0fc80
Matt Hayes <matt@mysterycommand.com>
2020-03-08 06:08:21 -0400

    feat: app controls/dice buttons

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,4 +2,30 @@ import React, { FC } from 'react';
 
 import './App.css';
 
-export const App: FC = () => <div className="App"></div>;
+export const App: FC = () => (
+  <div className="App">
+    <div className="App-controls">
+      <ul className="App-dice">
+        <li>
+          <button>d20</button>
+        </li>
+        <li>
+          <button>d12</button>
+        </li>
+        <li>
+          <button>d10</button>
+        </li>
+        <li>
+          <button>d8</button>
+        </li>
+        <li>
+          <button>d6</button>
+        </li>
+        <li>
+          <button>d4</button>
+        </li>
+      </ul>
+      <button>clear</button>
+    </div>
+  </div>
+);
