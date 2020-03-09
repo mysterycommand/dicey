cb6acb1d6c11a0b260416d69483bec777c5412fc
Matt Hayes <matt@mysterycommand.com>
2020-03-09 09:04:42 -0400

    Updates

---
+++ precache-manifest.3b6e20c15717447c9a40d71a9b1f0341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f273e03ae6ebee384bda05def41c07",
+    "url": "/dicey/index.html"
+  },
+  {
+    "revision": "47d432e6b9ad7ab982fd",
+    "url": "/dicey/static/css/main.700b635c.chunk.css"
+  },
+  {
+    "revision": "145d9eb192f1262e40b1",
+    "url": "/dicey/static/js/2.e8aa1ee7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/dicey/static/js/2.e8aa1ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47d432e6b9ad7ab982fd",
+    "url": "/dicey/static/js/main.1f10f6ad.chunk.js"
+  },
+  {
+    "revision": "f970fb40002991a37b83",
+    "url": "/dicey/static/js/runtime-main.0bf7c95c.js"
+  }
+]);
