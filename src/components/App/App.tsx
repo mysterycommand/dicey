c2281bf892ad3a9474af7fdced496f3cecbaaee6
Matt Hayes <matt@mysterycommand.com>
2020-03-15 23:58:40 -0400

    spike: messing with typing curry

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -12,6 +12,60 @@ type AppAction = {
   payload?: any;
 };
 
+// type MutatingKeys = 'splice' | 'push' | 'pop' | 'shift' | 'unshift';
+// type Fixed<T, L extends number, U = [...T[]]> = Pick<
+//   U,
+//   Exclude<keyof U, MutatingKeys>
+// > & {
+//   readonly length: L;
+//   [I: number]: T;
+//   [Symbol.iterator]: () => IterableIterator<T>;
+// };
+
+// type Tail<T extends any[]> = ((...args: T) => any) extends (
+//   _: any,
+//   ...tail: infer U
+// ) => any
+//   ? U
+//   : [];
+
+// type Length<T extends any[]> = T['length'];
+
+// type Prepend<E, T extends any[]> = ((head: E, ...args: T) => any) extends (
+//   ...args: infer U
+// ) => any
+//   ? U
+//   : T;
+
+// type Drop<N extends number, T extends any[], I extends any[] = []> = {
+//   0: Drop<N, Tail<T>, Prepend<any, I>>;
+//   1: T;
+// }[Length<I> extends N ? 1 : 0];
+
+type Cast<X, Y> = X extends Y ? X : Y;
+
+// type Curried<P extends any[], R> = <T extends any[]>(
+//   ...args: Cast<T, Partial<P>>
+// ) => Drop<Length<T>, P> extends [any, ...any[]]
+//   ? Curried<Cast<Drop<Length<T>, P>, any[]>, R>
+//   : R;
+
+// type Curried<F extends (...args: any) => any> = <T extends any[]>(
+//   ...args: Cast<Cast<T, Parameters<F>>, any[]>
+// ) => Parameters<F> extends [any, ...any[]]
+//   ? Curried<(...args: Cast<Parameters<F>, any[]>) => ReturnType<F>>
+//   : ReturnType<F>;
+
+type F = (...args: any) => any;
+const curry = (f: F, ...a: any[]) =>
+  f.length > a.length ? (...b: any[]) => curry(f, ...a.concat(b)) : f(...a);
+const add = (a: number, b: number) => a + b;
+
+const curriedAdd /* : Curried<typeof add> */ = curry(add);
+console.log(curriedAdd(1)(2));
+console.log(curriedAdd(3, 4));
+console.log(curriedAdd(5));
+
 const { floor, random } = Math;
 
 const rollDie = (sides: number) => floor(random() * sides) + 1;
